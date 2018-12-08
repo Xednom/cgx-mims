b83cc33707ffaf29f3c93d27a8385d29508920f5
Richmond Lacanlalay <monde.lacanlalay@gmail.com>
2018-12-08 21:21:16 +0800

    added: src, static, templates dir

---
+++ users/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext as _
+
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'first_name', 'last_name', 'date_of_birth']
+    UserAdmin.fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
