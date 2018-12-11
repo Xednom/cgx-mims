d9edd61e314a356160df1890555a44fd68773e7e
Josiah Regencia <jawshaeleazar@gmail.com>
2018-12-11 14:52:59 +0800

    [DEV] Added initial models

--- cgx/models.py
+++ cgx/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Agent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="Agent_id")
+    name = models.CharField(max_length=50, verbose_name="Agent_name")
+
+
+class Manager(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="Manager_id")
+    name = models.CharField(max_length=50, verbose_name="Manager_name")
+
+
+
+
