8e4d281ba2365aaf13de106415c6f1a825d2bc4a
Richmond Lacanlalay <monde.lacanlalay@gmail.com>
2019-01-07 19:16:34 +0800

    Updated: removed viewsets.py returned it to views

--- cgx_mims/routers.py
+++ cgx_mims/routers.py
@@ -1,9 +1,9 @@
 from rest_framework import routers
-from cgx.viewset import AgentViewSet, ManagerViewSet, BioConfirmMasterViewSet
+from cgx.views import AgentViewSet, ManagerViewSet, BioConfirmMasterViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register(r'agent', AgentViewSet)
 router.register(r'manager', ManagerViewSet)
-router.register(r'bioconfirmmaster', BioConfirmMasterViewSet)
+router.register(r'bio-confirm-master', BioConfirmMasterViewSet)
