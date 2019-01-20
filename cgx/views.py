5cc2934100e5f07dfb4bc920af1bd16b1d8fe0c5
Josiah Regencia <jawshaeleazar@gmail.com>
2019-01-21 05:39:10 +0800

    Initial for excel uploads

--- cgx/views.py
+++ cgx/views.py
@@ -47,7 +47,7 @@ def index(request):
 
 def upload_bcm(request):
     if "GET" == request.method:
-        return render(request, 'cgx/upload.html', {})
+        return render(request, 'cgx/upload_bcm.html', {})
     else:
         excel_file = request.FILES["excel_file"]
 
@@ -63,63 +63,54 @@ def upload_bcm(request):
             for cell in row:
                 row_data.append(str(cell.value))
             excel_data.append(row_data)
-        print(excel_data[0])
+        save_bcm(excel_data)
 
-        return render(request, 'cgx/upload.html', {"excel_data": excel_data})
-
-
-def upload_managers(request):
-    if "GET" == request.method:
-        return render(request, 'cgx/upload_managers.html', {})
-    else:
-        excel_file = request.FILES["excel_file"]
-
-        wb = openpyxl.load_workbook(excel_file)
-        worksheet = wb.active
-        print(worksheet)
-
-        excel_data = list()
-        # iterating over the rows and
-        # getting value from each cell in row
-        for row in worksheet.iter_rows():
-            row_data = list()
-            for cell in row:
-                row_data.append(str(cell.value))
-            excel_data.append(row_data)
-        print(excel_data[0])
-
-        for row in excel_data[1:]:
-            if not Manager.objects.filter(name=row[0]):
-                print("Row:", row[0])
-                manager = Manager(name=row[0])
-                print("Manager: ", manager)
-                serializer = ManagerSerializer(data=manager)
-                print("Data:", serializer.is_valid())
-                print("\n\n\n")
-
-                serializer.save()
-
-        return render(request, 'cgx/upload_managers.html', {"excel_data": excel_data})
+        return render(request, 'cgx/upload_bcm.html', {})
 
 
-def upload_agents(request):
-    if "GET" == request.method:
-        return render(request, 'cgx/upload_agents.html', {})
-    else:
-        excel_file = request.FILES["excel_file"]
+def save_bcm(excel_data):
+    for row in excel_data:
+        new_bcm = BioConfirmMaster(
+            patient_name=row[0],
+            patient_phone_number=row[1],
+            promo_code=row[2],
+            agent=add_agents(name=row[3]),
+            date_app_rec=row[5],
+            date_sample_rec=row[6],
+            type_of_test =row[7],
+            date_of_qca=row[8],
+            submitted_to_tamika_ins_verifier=row[9],
+            telemed_name=row[10],
+            date_submitted_to_telemed=row[11],
+            date_telemed_returned=row[12],
+            date_bioconfim_rec_app=row[13],
+            date_paid=row[15],
+            state=row[16],
+            status=row[17],
+            month=row[18],
+            insurance_company=row[19],
+            notes=row[20],
+            rejection_date=row[21],
+        )
 
-        wb = openpyxl.load_workbook(excel_file)
-        worksheet = wb.active
-        print(worksheet)
+        new_bcm.save()
 
-        excel_data = list()
-        # iterating over the rows and
-        # getting value from each cell in row
-        for row in worksheet.iter_rows():
-            row_data = list()
-            for cell in row:
-                row_data.append(str(cell.value))
-            excel_data.append(row_data)
-        print(excel_data[0])
 
-        return render(request, 'cgx/upload_agents.html', {"excel_data": excel_data})
+def add_managers(manager):
+
+    if not Manager.objects.filter(name=manager):
+        print("Row:", manager)
+        new_manager = Manager(name=manager)
+        new_manager.save()
+
+        return new_manager
+
+
+def add_agents(agent):
+
+    if not Agent.objects.filter(name=agent):
+        print("Row:", agent)
+        new_agent = Manager(name=agent)
+        new_agent.save()
+
+        return Agent.objects.get(name=new_agent.name)
