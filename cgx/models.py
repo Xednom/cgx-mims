3aaaedb6ca73ad4f06509c0f4c96af51c0b750f0
Josiah Regencia <jawshaeleazar@gmail.com>
2019-01-21 14:06:20 +0800

    Updates in CGX models

--- cgx/models.py
+++ cgx/models.py
@@ -12,26 +12,26 @@ CGX_CARRIER = 3
 L = 4
 
 # STATUS_CHOICES
-CGX_CGD = 1
-DUPLICATE = 2
-HOLD = 3
-MISSING_INFORMATION = 4
-REJECTED = 5
-WRONG_APPLICATION = 6
-RFE = 7
-RESIGNED = 8
-RA = 9
-REJ_HMO = 10
-REJ_NO_ID = 11
-REJ_INELIGIBLE = 12
-REJ_INACTIVE = 13
-REJ_HIGD = 14
-REJ_CARESOURCE = 15
+CGX_CGD = "CGX/CGD"
+DUPLICATE = "DUPLICATE"
+HOLD = "HOLD"
+MISSING_INFORMATION = "MISSING INFORMATION"
+REJECTED = "REJECTED"
+WRONG_APPLICATION = "WRONG APPLICATION"
+RFE = "Resubmit from Elite"
+RESIGNED = "RESIGNED"
+RA = "Requires Authorization"
+REJ_HMO = "REJECTED HMO"
+REJ_NO_ID = "REJECTED NO ID"
+REJ_INELIGIBLE = "REJECTED INELIGIBLE"
+REJ_INACTIVE = "REJECTED INACTIVE"
+REJ_HIGD = "REJECTED - Higd ded 6880.37"
+REJ_CARESOURCE = "REJECTED CARESOURCE"
 
 # REASON_FOR_REJECTIONS_CHOICES
 HD = 1
 HIPAA = 2
-RA = 3
+RRA = 3
 
 
 TYPE_OF_TEST_CHOICES = (
@@ -52,7 +52,7 @@ STATUS_CHOICES = (
     (RFE, "Resubmit from Elite"),
     (RESIGNED, "RESIGNED"),
     (REJ_HMO, "REJECTED HMO"),
-    (REJ_NO_ID, "REJECTED - NO ID"),
+    (REJ_NO_ID, "REJECTED NO ID"),
     (REJ_INELIGIBLE, "REJECTED INELIGIBLE"),
     (REJ_INACTIVE, "REJECTED INACTIVE"),
     (REJ_HIGD, "REJECTED - Higd ded 6880.37"),
@@ -62,7 +62,7 @@ STATUS_CHOICES = (
 REASON_FOR_REJECTIONS_CHOICES = (
     (HD, "HIGH DEDUCTIBLE - 2400.00"),
     (HIPAA, "HIPAA VIOLATION"),
-    (RA, "REQUIRES AUTHENTICATION")
+    (RRA, "REQUIRES AUTHENTICATION")
 )
 
 
@@ -115,7 +115,7 @@ class BioConfirmMaster(models.Model):
     date_bioconfim_rec_app = models.DateField(default=timezone.now, verbose_name="Date bioconfirm recorded application", null=True, blank=True)
     date_paid = models.DateField(default=timezone.now, verbose_name="Date paid", null=True, blank=True)
     state = models.CharField(max_length=50, verbose_name="State", null=True, blank=True)
-    status = models.IntegerField(choices=STATUS_CHOICES, verbose_name="Status", null=True, blank=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50, verbose_name="Status", null=True, blank=True)
     month = models.DateField(null=True, blank=True)
     insurance_company = models.CharField(max_length=50, verbose_name="Insurance company name", null=True, blank=True)
     notes = models.TextField(verbose_name="Notes", null=True, blank=True)
