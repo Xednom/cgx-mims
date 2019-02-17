c71d09b915b27f9a39e9ecce150de371f33a6d60
Mae Sace <felicia.mae.sace@dynameyes.com>
2019-02-17 23:58:07 +0800

    Add pagination to bioconfirm page

--- src/js/tsg/bioconfirm.js
+++ src/js/tsg/bioconfirm.js
@@ -36,7 +36,14 @@ new Vue({
       'consent_recording': "",
     },
     search_term: '',
-    fileInput: {},
+
+    // for pagination
+    currentPage: 1,
+    pageSize: RECORDS_PER_PAGE,
+    startPage: 1,
+    endPage: null,
+    maxPages: RECORDS_PER_PAGE,
+    paginatedRecords: [],
   },
   mounted: function() {
     this.getBioConfirms();
@@ -135,16 +142,81 @@ new Vue({
     },
     // viewing of full datas
     viewBioConfirm: function(id) {
-    this.loading = true;
-    this.$http.get(`/api/v1/bio-confirm-master/${id}/`)
-        .then((response) => {
-          this.loading = false;
-          this.currentBioConfirm = response.data;
-        })
-        .catch((err) => {
-          this.loading = false;
-          console.log(err);
-        })
+      this.loading = true;
+      this.$http.get(`/api/v1/bio-confirm-master/${id}/`)
+          .then((response) => {
+            this.loading = false;
+            this.currentBioConfirm = response.data;
+          })
+          .catch((err) => {
+            this.loading = false;
+            console.log(err);
+          })
     },
-  }
+    getPaginatedRecords: function() {
+      const startIndex = this.startIndex;
+      const endIndex = this.endIndex;
+      this.paginatedRecords = this.bioconfirms.slice().splice(startIndex, this.pageSize);
+    },
+    goToPage: function(page) {
+      if (page < 1) {
+        return this.currentPage = 1;
+      }
+      if (page > this.totalPages) {
+        return this.currentPage = this.totalPages;
+      }
+      this.currentPage = page;
+    },
+    setPageGroup: function() {
+      if (this.totalPages <= this.maxPages) {
+        this.startPage = 1;
+        this.endPage = Math.min(this.totalPages, this.maxPages);
+      } else {
+        let maxPagesBeforeCurrentPage = Math.floor(this.maxPages / 2);
+        let maxPagesAfterCurrentPage = Math.ceil(this.maxPages / 2) - 1;
+        if (this.currentPage <= maxPagesBeforeCurrentPage) {
+            // current page near the start
+            this.startPage = 1;
+            this.endPage = this.maxPages;
+        } else if (this.currentPage + maxPagesAfterCurrentPage >= this.totalPages) {
+            // current page near the end
+            this.startPage = this.totalPages - this.maxPages + 1;
+            this.endPage = this.totalPages;
+        } else {
+            // current page somewhere in the middle
+            this.startPage = this.currentPage - maxPagesBeforeCurrentPage;
+            this.endPage = this.currentPage + maxPagesAfterCurrentPage;
+        }
+      }
+    }
+  },
+  watch: {
+    bioconfirms: function(newBioconfirmRecords, oldBioconfirmRecords) {
+      this.setPageGroup();
+      this.getPaginatedRecords();
+    },
+    currentPage: function(newCurrentPage, oldCurrentPage) {
+      this.setPageGroup();
+      this.getPaginatedRecords();
+    },
+  },
+  computed: {
+    totalItems: function() {
+      return this.bioconfirms.length;
+    },
+    totalPages: function() {
+      return Math.ceil(this.totalItems / this.pageSize);
+    },
+    startIndex: function() {
+      return (this.currentPage - 1) * this.pageSize;
+    },
+    endIndex: function() {
+      return Math.min(this.startIndex + this.pageSize - 1, this.totalItems - 1);
+    },
+    pages: function() {
+      let pages = [];
+      for (let i = this.startPage; i < this.endPage; i++) pages.push(i);
+      return pages;
+    },
+  },
 });
